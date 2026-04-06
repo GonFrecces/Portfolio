82cb376438418ce829d4c958528ace8fd542eece
GonFrecces <54601577+GonFrecces@users.noreply.github.com>
2026-04-06 10:12:51 -0400

    feat(add workflow github)

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isGitHubActions = process.env.GITHUB_ACTIONS === "true"
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? ""
+
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  basePath: isGitHubActions && repositoryName ? `/${repositoryName}` : "",
+  assetPrefix: isGitHubActions && repositoryName ? `/${repositoryName}/` : "",
   typescript: {
     ignoreBuildErrors: true,
   },
